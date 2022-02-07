ec40610bcea0bc6382ad819a25bc22f3099c6b4c
AtIasShrugged <btw210899@gmail.com>
2022-02-07 21:50:21 +0200

    Updated App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import { Button } from 'antd';
 import { useState } from 'react';
 import './App.css';
+import RepositoryItemForm from './components/RepositoryItemForm';
+import RepositoryList from './components/RepositoryList';
+import Sync from './components/Sync';
 
 function App() {
-	const [repositories, setRepositories] = useState([]);
+	const arr = ['Get all repositories', 'Get one repository', 'Sync'];
+	const [currentMenu, setCurrentMenu] = useState(arr[0]);
+
+	const changeMenuMode = (e) => {
+		setCurrentMenu(e.target.value);
+	};
 
 	return (
 		<div className='container'>
-			<Button type='primary'>Load repositoryes</Button>
-			<Button type='primary'>Load one repository</Button>
-			<Button type='primary'>Sync internal timer</Button>
+			<select className='elem' defaultValue={currentMenu} onChange={changeMenuMode}>
+				{arr.map((menu) => (
+					<option key={menu} value={menu}>
+						{menu}
+					</option>
+				))}
+			</select>
+			{currentMenu === 'Get all repositories' ? <RepositoryList /> : null}
+			{currentMenu === 'Get one repository' ? <RepositoryItemForm /> : null}
+			{currentMenu === 'Sync' ? <Sync /> : null}
 		</div>
 	);
 }
